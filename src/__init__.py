8e9f6f00a387f7f7497d04728041cf6f6768930f
Mohamad Ashraf Salama <mohamadashrafaast@gmail.com>
2022-08-04 20:23:51 +0000

    add package docstring and version to __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+SIC/XE Two-Pass Assembler
+
+A Python implementation of a two-pass assembler for the SIC/XE
+(Simplified Instructional Computer with Extra Equipment) architecture.
+"""
+
+__version__ = '1.0.0'
